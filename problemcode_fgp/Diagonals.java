e76e887933bee12e0c1758d7262b862e81755725
jbsp3416-uol <jbsp3416@leeds.ac.uk>
2026-04-06 21:16:41 +0000

    Diagonals function layout set up

--- problemcode_fgp/Diagonals.java
+++ problemcode_fgp/Diagonals.java
@@ -2,5 +2,23 @@ import static java.lang.Math.*;
 
 class Diagonals
 {
+    static double[] exampleMatrix(int n) // Member 3 Function
+    {
+        return 0;
+    }
 
+    static double[] sum(double[], double[]) // Member 2 Function
+    {
+        return 0;
+    }
+
+    static double[] product(double[], double[]) // Member 2 Function
+    {
+        return 0;
+    }
+
+    static double[] inverse(double[]) // Member 1 Function
+    {
+        return 0;
+    }
 }
