c596495b2a1b86459d8a628f6aeebe7cfc9a8e7e
jbsp3416-uol <jbsp3416@leeds.ac.uk>
2026-04-06 21:11:08 +0000

    Tridiagonals function layout set up

--- problemcode_fgp/Tridiagonals.java
+++ problemcode_fgp/Tridiagonals.java
@@ -2,9 +2,29 @@ import static java.lang.Math.*;
 
 class Tridiagonals
 {
-static double[][] exampleMatrix(int n) 
+    static double[][] exampleMatrix(int n) //Member 2 Function
     {
         double m[][] = new double [1][1]; //dummy code: write your own
         return m; //dummy code: write your own
+    }
+
+    static boolean isValidTridiagonal(double[][]) // Member 3 Function
+    {
+        return 0;
+    }
+
+    static double[][] sum(double[][], double[][]) // Member 1 Function
+    {
+        return 0;
+    }
+
+    static double[][] productWithDiagonal(double[], double[][]) // Member 2 Function
+    {
+        return 0;
+    }
+
+    static double[] linearSolve(double[][], double[]) // Member 3 Function
+    {
+        return 0;
     }
 }
