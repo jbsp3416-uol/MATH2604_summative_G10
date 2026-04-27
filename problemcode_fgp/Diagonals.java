6fc9f7a4fa2640df3feb3ced420f8cf00403a2af
Temitayo Olowolagba <zxnx2440@leeds.ac.uk>
2026-04-27 22:45:49 +0100

    simplified sum/product input names and fixed parameter types for testing the functions

--- problemcode_fgp/Diagonals.java
+++ problemcode_fgp/Diagonals.java
@@ -2,72 +2,38 @@ import static java.lang.Math.*;
 
 class Diagonals
 {
-//
+//part(a)
 static double[] exampleMatrix(int n) 
 {
     // Member 3 Function (n/a)
     return null;
 }
 
-//1(b) sum of two diagonal matrices
-public static double[] sum(double[] matrixA, double[] matrixB) {
-    int l = matrixA.length;
+//part(b) sum of two diagonal matrices
+public static double[] sum(double[] A, double[] B) {
+    int l = A.length;
     double[] result = new double[l];
 
     for (int i = 0; i < l; i++) {
-        result[i] = matrixA[i] + matrixB[i];
+        result[i] = A[i] + B[i];
     }
 
     return result;
 }
 
-//1(c) product of two diagonal matrices
-public static double[] product(double[] matrixA, double[] matrixB) {
-    int l = matrixA.length;
+//part(c) product of two diagonal matrices
+public static double[] product(double[] A, double[] B) {
+    int l = A.length;
     double[] result = new double[l];
 
     for (int i = 0; i < l; i++) {
-        result[i] = matrixA[i] * matrixB[i];
+        result[i] = A[i] * B[i];
     }
 
     return result;
 }
-//function to print matrix
-public static void printMatrix(double[] matrix)
-    {
-        for (double value : matrix) {
-            System.out.print(value + " ");
-        }
-    }
 
-public static void main(String[] args) {
-    //testing the sum of two matrices
-    double[][] A = {
-        {1, 0, 0},
-        {0, 2, 0},
-        {0, 0, 3}
-    };
-
-    double[][] B = {
-        {3, 0, 0},
-        {0, 2, 0},
-        {0, 0, 1}
-    };
-
-    double[][] sumResult = sum(A, B);
-    
-    System.out.println("Sum of matrices:");
-    printMatrix(sumResult);
-    
-    System.out.println(); //to separate outputs for clarity
-    
-    //testing product function
-    double[][] productResult = product(A,B);
-    System.out.println("Product of matrices:");
-    printMatrix(productResult);
-}
-
- // 1(d) inverse of a diagonal matrix
+ // part(d) inverse of a diagonal matrix
 public static double[] inverse(double[] A) 
 {
     int l = A.length;
@@ -77,4 +43,33 @@ public static double[] inverse(double[] A)
     }
     return inverse;
 }
+
+//function to print matrix
+public static void printMatrix(double[] matrix)
+    {
+        for (double value : matrix) {
+            System.out.print(value + " ");
+        }
+    }
+
+//to test each function
+public static void main(String[] args) {
+    //part(b) testing the sum of two matrices
+    double[] A = {1, 2, 3};
+
+    double[] B = {4, 5, 6};
+
+    double[] sumResult = sum(A, B);
+    
+    System.out.println("Sum of matrices:");
+    printMatrix(sumResult);
+    
+    System.out.println(); //to separate outputs for clarity
+    
+    //part(c) testing product function
+    double[] productResult = product(A,B);
+    System.out.println("Product of matrices:");
+    printMatrix(productResult);
+}
+
 }
